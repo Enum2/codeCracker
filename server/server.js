c4b327f7ebdfc9e81f4b4ad0cf0b9b672dabdf83
Enum2 <sujalbs2018@gmail.com>
2025-03-02 17:41:58 +0530

    server created

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("hello");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
