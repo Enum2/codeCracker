6cba9206429dc3f5a56754a7c12934da09e0f68d
Enum2 <sujalbs2018@gmail.com>
2025-03-02 17:53:15 +0530

    database  created

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./utils/DB.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is listening on port ${PORT}`);
+  await connectDB();
 });
